3968b3b4913b46821cd08a8b45e910e7b7404660
ManasaGDL <maanasa.lakshmi22@gmail.com>
2026-01-30 21:37:42 +0530

    added frontend varaible to BE conf

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,12 @@ if (process.env.NODE_ENV !== "production") {
 
 const app = express();
 
-app.use(cors());
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL,
+        credentials: true,
+    })
+);
 app.use(express.json());
 
 // ✅ Routes
