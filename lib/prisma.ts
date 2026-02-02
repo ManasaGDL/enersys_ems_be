dff7e8d5c444377b853e6de2a2ff5440411d38f1
ManasaGDL <maanasa.lakshmi22@gmail.com>
2026-02-02 12:30:15 +0530

    consolelog in prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,7 +3,7 @@ import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from "@prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`
-
+console.log("connectionStribg", connectionString)
 const adapter = new PrismaPg({ connectionString })
 const prisma = new PrismaClient({ adapter })
 
